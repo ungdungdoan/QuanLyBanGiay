b6df2f72ecd1066999ec8ef8e8c5a827f0ae302b
ungdungdoan <ungdungdoan@gmail.com>
2016-11-12 23:19:11 +0700

    Add files via upload

--- DATABASE.sql
+++ DATABASE.sql
@@ -8,3 +8,12 @@ create table QLgiay(
   )
 
 select* from QLgiay
+insert into QLgiay values('abc', '1234','xanhla','20','4','200000')
+insert into QLgiay values('xyz', '2345','xanhduong','40','2','3000000')
+insert into QLgiay values('ngauhung', '4657','xanhlam','39,40,41','2','555000')
+
+
+delete QLgiay where ma=2132332 
+delete QLgiay where ma=233
+delete QLgiay where ma = 1212
+delete QLgiay where ma=23
